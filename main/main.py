13c016d3ab0251b3720706e0bcb5c3b1c93ca5fe
Gabriel Victor <122997003+GabrielHidaN@users.noreply.github.com>
2024-02-06 22:02:23 -0300

    add.FirstsArchive

--- main/main.py
+++ main/main.py
@@ -1,3 +1,7 @@
 import sys
 import os
 import pyfiglet
+
+from name_ascii import*
+
+ascii_text()
---
+++ main/name_ascii.py
@@ -0,0 +1,7 @@
+import pyfiglet
+def ascii_text():
+  TEXT = "HidanX"
+  ASCII_art_1 = pyfiglet.figlet_format(TEXT)
+  print(ASCII_art_1)
+
+
