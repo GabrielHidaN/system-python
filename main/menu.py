5662e0258df2c44fa480b15b5e28c38438b5396e
Gabriel Victor <122997003+GabrielHidaN@users.noreply.github.com>
2024-02-18 23:41:15 -0300

    add items menu

--- main/menu.py
+++ main/menu.py
@@ -10,6 +10,7 @@ def menu():
   print(ascii_art_menu)
   print(paragrafo_menu)
   print('')
-  secao1 = ' (1)Manipulação de Dados\n (2)....\n (3)...\n (4)...\n (5)...\n (6)...  \n (7)...\n (8)...\n (9)...\n (0)voltar\n'
+  secao1 = ' (1)Manipulação de Dados\n (2)Automatizações\n (3)Games\n (4)IA\n (5)Buscador de Vagas\n (6)...  \n (7)...\n (8)...\n (9)...\n (0)voltar\n'
 
   print(secao1)
+  print(ascii_art_hidanx)
