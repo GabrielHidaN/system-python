592f1333687ce9c136e1dfd23f241a2467bc3315
Gabriel Victor <122997003+GabrielHidaN@users.noreply.github.com>
2024-02-20 22:32:41 -0300

    ADD CONST

--- main/menu.py
+++ main/menu.py
@@ -7,10 +7,10 @@ from init import*
 def menu():
   print('')
   print(paragrafo1)
-  print(ascii_art_menu)
+  print(ASCII_ART_MENU)
   print(paragrafo_menu)
   print('')
   secao1 = ' (1)Manipulação de Dados\n (2)Automações\n (3)Games\n (4)IA\n (5)Buscador de Vagas\n (6)...  \n (7)...\n (8)...\n (9)...\n (0)voltar\n'
 
   print(secao1)
-  print(ascii_art_hidanx)
+  print(ASCII_NAME_HIDANX)
--- main/name_ascii.py
+++ main/name_ascii.py
@@ -1,9 +1,9 @@
 import pyfiglet
 
-ascii_art_hidanx = pyfiglet.figlet_format("HidanX", font = "slant"  )
+ASCII_NAME_HIDANX = pyfiglet.figlet_format("HidanX", font = "slant"  )
 
-menu_text = "MENU"
-ascii_art_menu = pyfiglet.figlet_format(menu_text)
+MENU_TEXT = "MENU"
+ASCII_ART_MENU = pyfiglet.figlet_format(MENU_TEXT)
 
 paragrafo_menu = 'Encontre aplicativos, configurações e arquivos com facilidade'
 paragrafo1 = ' Seja bem-vindo ao nosso sistema!\n Estamos empolgados por tê-lo(a) conosco,\n e queremos proporcionar uma experiência excepcional.\n Estamos aqui para facilitar sua jornada e garantir que aproveite ao máximo todas as funcionalidades disponíveis.'
